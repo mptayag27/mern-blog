a2449b66d52cb51e874231fff90d0b6f13c11a79
mptayag27 <mptayag27@gmail.com>
2022-06-29 10:56:08 -0700

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,1 +1,11 @@
-lll
+//server.js
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+
+const app = express();
+
+app.use(bodyParser.json({ limit: '50mb', extended: true }))
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }))
+app.use(cors());
