9436e9abd67088bce068bab9dcd3281782d07f90
mptayag27 <mptayag27@gmail.com>
2022-06-29 20:16:05 -0700

    ups

--- server/server.js
+++ server/server.js
@@ -3,9 +3,24 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 
-app.use(bodyParser.json({ limit: '50mb', extended: true }))
-app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }))
+app.use(bodyParser.json({ limit: "50mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 app.use(cors());
+
+const DB_CONNECTION = "mongodb+srv://mptayag:0IheWbvs9NGkAmDH@cluster0.hre9x.mongodb.net/?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 6000;
+
+mongoose
+  .connect(DB_CONNECTION, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() =>
+    app.listen(PORT, () =>
+      console.log(`Server is running @ : http://localhost:${PORT}`)
+    )
+  )
+  .catch((error) => console.error(error));
