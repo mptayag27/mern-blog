e32c6905640aa8a22b1f9bf453b0820021b45a4e
mptayag27 <mptayag27@gmail.com>
2022-06-29 20:51:52 -0700

    m

--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
-//server.js
+// server.js
 import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
+
+import blogPosts from "./routes/blogPosts.js";
 
 dotenv.config();
 
@@ -13,14 +15,20 @@ app.use(bodyParser.json({ limit: "50mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 app.use(cors());
 
-const DB_CONNECTION = "mongodb+srv://mptayag:0IheWbvs9NGkAmDH@cluster0.hre9x.mongodb.net/?retryWrites=true&w=majority";
+// remember to add this after cors
+app.use("/api/blogs", blogPosts);
+
+const DB_CONNECTION = "mongodb+srv://mptayag:0IheWbvs9NGkAmDH@cluster0.hre9x.mongodb.net/?retryWrites=true&w=majority"
 const PORT = process.env.PORT || 6000;
 
 mongoose
-  .connect(DB_CONNECTION, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(DB_CONNECTION, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() =>
     app.listen(PORT, () =>
-      console.log(`Server is running @ : http://localhost:${PORT}`)
+      console.log(`Server is running at: http://localhost:${PORT}`)
     )
   )
-  .catch((error) => console.error(error));
+  .catch((error) => console.log(error));
